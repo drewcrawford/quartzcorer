76c2113c4e740ab50ef37c646bc86316c98ef460
Drew Crawford <drew@sealedabstract.com>
2022-07-11 18:19:03 -0500

    [CALayer] contentsScale

--- src/calayer.rs
+++ src/calayer.rs
@@ -1,6 +1,41 @@
+use coregraphicsr::CGFloat;
 use objr::bindings::*;
+use crate::CAMetalLayer;
 objc_class! {
     pub struct CALayer {
         @class(CALayer)
+    }
+}
+
+objc_selector_group! {
+    trait Selectors {
+        @selector("contentsScale")
+        @selector("setContentsScale:")
+    }
+    impl Selectors for Sel {}
+}
+
+#[allow(non_snake_case)]
+impl CALayer {
+    pub fn contentsScale(&self, pool: &ActiveAutoreleasePool) -> CGFloat {
+        unsafe {
+            Self::perform_primitive(self.assume_nonmut_perform(), Sel::contentsScale(), pool, ())
+        }
+    }
+    pub fn setContentsScale(&mut self, scale: CGFloat, pool: &ActiveAutoreleasePool) {
+        unsafe {
+            Self::perform_primitive(self, Sel::setContentsScale_(), pool, (scale,))
+        }
+    }
+}
+
+impl From<&CAMetalLayer> for &CALayer {
+    fn from(a: &CAMetalLayer) -> Self {
+        unsafe{&* (a as *const _ as *const CALayer)}
+    }
+}
+impl From<&mut CAMetalLayer> for &mut CALayer {
+    fn from(a: &mut CAMetalLayer) -> Self {
+        unsafe{&mut * (a as *mut _ as *mut CALayer)}
     }
 }
